24ec8bf444c1368c10093ff66f971539967ffdd2
Manuel <manuelinfosec@gmail.com>
2024-06-15 00:56:00 +0100

    (parsers): parsing command-line arguments

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,23 @@
-use clap::Parser;
+use clap::{arg, ArgAction, Parser};
 
 #[derive(Parser, Debug)]
-#[command(version, about = "DESCRIPTION")]
-struct Command {
+#[command(
+    version,
+    about = "touch - change file timestamps\n\nUpdate the access and modification times of each FILE to the current time.\nA FILE argument that does not exist is created empty, unless -c is supplied.\nMandatory arguments to long options are mandatory for short options too."
+)]
+pub struct Command {
+    #[arg(short, action = ArgAction::SetTrue, help = "change file access time")]
+    access: bool,
+
+    #[arg(
+        short,
+        long,
+        help = "specifies the date to use instead of current time"
+    )]
+    date: Option<String>,
+
+    #[arg(short, action = ArgAction::SetTrue, help = "change file modification time")]
+    modify: bool,
+
     pub file_paths: Vec<String>,
 }
